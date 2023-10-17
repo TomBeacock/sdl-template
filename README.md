678916b351d0b03bbbd8b173a931208898046bc1
Thomas Beacock <thomasbeacock@gmail.com>
2023-10-17 17:28:21 +0100

    Added Vulkan setup and pipeline

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Template for SDL Projects
+
+CMake project setup with SDL3 statically linked. Additionally includes Vulkan SDK setup and basic graphics pipeline.
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,54 @@
-#include "SDL.h"
+#include "vulkan_main.h"
 
-int main(int argc, char** argv)
+#include <SDL.h>
+#include <SDL_main.h>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <vulkan/vulkan.h>
+
+int SDL_main(int argc, char **argv)
 {
-	SDL_Init(0);
-	SDL_Window* window = SDL_CreateWindow("SDL Template", 1280, 720, SDL_WINDOW_RESIZABLE);
-	SDL_ShowWindow(window);
+    SDL_Init(SDL_INIT_VIDEO);
 
-	bool running = true;
-	SDL_Event e;
-	while (running) {
-		SDL_PollEvent(&e);
-		switch (e.type) {
-		case SDL_EVENT_MOUSE_WHEEL: {
-			SDL_Log("%f, %f", e.wheel.x, e.wheel.y);
-			break;
-		}
-		case SDL_EVENT_QUIT: {
-			running = false;
-			break;
-		}
-		}
-	}
+    SDL_Window *window = SDL_CreateWindow(
+        "Template", 1280, 720, SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN);
 
-	SDL_DestroyWindow(window);
+    Vulkan *vulkan;
+    try {
+        vulkan = new Vulkan(window);
+    } catch (std::runtime_error e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    SDL_ShowWindow(window);
+
+    bool running = true;
+    SDL_Event e;
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+            case SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED: {
+                vulkan->framebuffer_size_changed();
+                break;
+            }
+            case SDL_EVENT_QUIT: {
+                running = false;
+                break;
+            }
+            }
+        }
+
+        bool minimized = SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED;
+        if (!minimized) {
+            vulkan->draw_frame();
+        }
+    }
+
+    vulkan->wait_till_idle();
+    delete vulkan;
+
+    SDL_DestroyWindow(window);
+
+    return 0;
 }
